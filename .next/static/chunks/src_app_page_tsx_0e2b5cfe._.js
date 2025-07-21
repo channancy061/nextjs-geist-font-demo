e65ec39f26010912cd0ac9eda992569a2545151f
channancy061 <channancy061+github@gmail.com>
2025-07-22 00:13:38 +0530

    Add .next/static/chunks/src_app_page_tsx_0e2b5cfe._.js 1753123418503

---
+++ .next/static/chunks/src_app_page_tsx_0e2b5cfe._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_bec5c460._.js",
+  "static/chunks/node_modules_f44fa066._.js"
+],
+    source: "dynamic"
+});
